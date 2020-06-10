6c8e4f940c4f36125cab6fa82b88d77d4b4cb8ef
DTOM TAROMB <trmmail@gmail.com>
2020-06-11 02:34:51 +0700

    Update snapmirrorupdate.sh

--- Ver3/snapmirrorupdate.sh
+++ Ver3/snapmirrorupdate.sh
@@ -1,3 +1,19 @@
-ssh admin@192.168.0.110 snapmirror update svm2:svm1_lun1_dest
-ssh admin@192.168.0.110 snapmirror update svm2:svm1_lun2_dest
-sleep 5
+## DETECTING VMX FILE ###
+cat vars.yaml | shyaml get-value ontap_drvolumes| awk '{print $2}' > vol_dest.list
+ontap_drhostname=`cat vars.yaml | shyaml get-value ontap_drhostname`
+ontap_drusername=`cat vars.yaml | shyaml get-value ontap_drusername`
+
+FILEIN=vol_dest.list
+LOOPS=`wc -l $FILEIN |awk '{print $1}'`
+echo $LOOPS
+COUNT=1
+
+## LOOP TO REGISTER VMX ON ALL DEVICE ###
+while [ $COUNT -le $LOOPS ]
+do
+VOL_DEST=`head -$COUNT $FILEIN |tail -1|awk '{print $1}'`
+ssh $ontap_drusername@$ontap_drhostname "snapmirror update" $VOL_DEST
+sleep 3
+(( COUNT++ ))
+done
+
