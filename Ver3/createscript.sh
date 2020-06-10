10deb506f45e7d5f7fdd3c521c61bf3e59f11805
Supawat <supawat.nick@gmail.com>
2020-06-10 12:21:07 +0700

    ok

--- Ver3/createscript.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export ANSIBLE_LOG_PATH=log/create-`date +%Y%m%d%H%M%S`.log
-cd /netapp
-ansible-playbook create_cgsnap.yaml 
--- Ver3/deletescript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export ANSIBLE_LOG_PATH=log/delete-`date +%Y%m%d%H%M%S`.log
-
-cd /netapp
-ansible-playbook delete_cgsnap.yaml
