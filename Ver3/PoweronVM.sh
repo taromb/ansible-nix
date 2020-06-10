18b2cd410d383d1223ef8d1ab3699fa2ba2b1d5e
Supawat <supawat.nick@gmail.com>
2020-06-10 23:20:36 +0700

    Create PoweronVM.sh

---
+++ Ver3/PoweronVM.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+## DETECTING VM ID ###
+ssh root@192.168.0.103 "vim-cmd vmsvc/getallvms | grep snap" > getallvms.list
+FILEIN=getallvms.list
+LOOPS=`wc -l $FILEIN |awk '{print $1}'`
+echo $LOOPS
+COUNT=1
+
+## LOOP TO POWER ON VM ###
+while [ $COUNT -le $LOOPS ]
+do
+VM_ID=`head -$COUNT $FILEIN |tail -1|awk '{print $1}'`
+ssh root@192.168.0.103 "vim-cmd vmsvc/power.onm " $VM_ID
+(( COUNT++ ))
+done
+
+## CLEAN DATA FILE ##
+rm getallvms.list
