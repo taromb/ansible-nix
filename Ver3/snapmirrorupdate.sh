75d09a78846f597c78cda772d252d2bc823917af
Supawat <supawat.nick@gmail.com>
2020-06-10 23:12:24 +0700

    Update snapmirrorupdate.sh

--- Ver3/snapmirrorupdate.sh
+++ Ver3/snapmirrorupdate.sh
@@ -1,3 +1,3 @@
-ssh admin@192.168.0.110 snapmirror update svm2:lun1_vol_dest
-ssh admin@192.168.0.110 snapmirror update svm2:lun2_vol_dest
+ssh admin@192.168.0.110 snapmirror update svm2:svm1_lun1_dest
+ssh admin@192.168.0.110 snapmirror update svm2:svm1_lun2_dest
 sleep 5
